dbcb0743352f5a84351c33182849cfca72f594b9
Jorge Díaz <joandiar91@gmail.com>
2023-11-05 20:30:14 -0500

    Clase 50. Iteradores para las tarjetas de mokepones

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -20,6 +20,7 @@ const spanVidasEnemigo = document.getElementById("vidas-enemigo")
 const sectionMensajes = document.getElementById('resultado')
 const ataquesDelJugador = document.getElementById('ataques-del-jugador')
 const ataquesDelEnemigo = document.getElementById('ataques-del-enemigo')
+const contenedorTarjetas = document.getElementById('contenedorTarjetas')
 
 let mokepones = []
 
@@ -65,8 +66,23 @@ ratigueya.ataques.push(
     {nombre: '🌱', id: 'boton-tierra'},
 )
 
+mokepones.push(hipodoge, capipepo, ratigueya)
+
 function iniciarJuego() {
     sectionSeleccionarAtaque.style.display = 'none'
+
+    mokepones.forEach((mokepon) => {
+        opcionDeMokepones = `
+            <input type="radio" name="mascota" id=${mokepon.nombre}>
+            <label class="tarjeta-de-mokepon" for=${mokepon.nombre}>
+                <p>${mokepon.nombre}</p>
+                <img src=${mokepon.foto} alt=${mokepon.nombre}>
+            </label>
+        `
+
+        contenedorTarjetas.innerHTML += opcionDeMokepones
+    })
+
     sectionReiniciar.style.display = 'none'
 
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
