6cf1592b10b7cc7640a9e74d9cd14cfbf50fba7b
Jorge Díaz <joandiar91@gmail.com>
2024-01-06 16:50:18 -0500

    Clase 66: Métodos en las clases. Mascotas enemigos

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -46,25 +46,39 @@ let mapaBackground = new Image()
 mapaBackground.src = './assets/mokemap.png'
 
 class Mokepon {
-    constructor(nombre, foto, vida) {
+    constructor(nombre, foto, vida, fotoMapa, x = 10, y = 10) {
         this.nombre = nombre
         this.foto = foto
         this.vida = vida
         this.ataques = []
-        this.x = 20
-        this.y = 30
-        this.ancho = 80
-        this.alto = 80
+        this.x = x
+        this.y = y
+        this.ancho = 40
+        this.alto = 40
         this.mapaFoto = new Image()
-        this.mapaFoto.src = foto
+        this.mapaFoto.src = fotoMapa
         this.velocidadX = 0
         this.velocidadY = 0
     }
+
+    pintarMokepon() {
+        lienzo.drawImage(
+            this.mapaFoto,
+            this.x,
+            this.y,
+            this.ancho,
+            this.alto
+        )
+    }
 }
 
-let hipodoge = new Mokepon ('Hipodoge', './assets/hipodoge.png', 5)
-let capipepo = new Mokepon ('Capipepo', './assets/capipepo.png', 5)
-let ratigueya = new Mokepon ('Ratigueya', './assets/ratigueya.png', 5)
+let hipodoge = new Mokepon ('Hipodoge', './assets/hipodoge.png', 5, './assets/hipodoge_head.png')
+let capipepo = new Mokepon ('Capipepo', './assets/capipepo.png', 5, './assets/capipepo_head.png')
+let ratigueya = new Mokepon ('Ratigueya', './assets/ratigueya.png', 5, './assets/ratigueya_head.png')
+
+let hipodogeEnemigo = new Mokepon ('Hipodoge', './assets/hipodoge.png', 5, './assets/hipodoge_head.png', 180, 430)
+let capipepoEnemigo = new Mokepon ('Capipepo', './assets/capipepo.png', 5, './assets/capipepo_head.png', 150, 95)
+let ratigueyaEnemigo = new Mokepon ('Ratigueya', './assets/ratigueya.png', 5, './assets/ratigueya_head.png' , 710, 400)
 
 hipodoge.ataques.push(
     {nombre: '💧', id: 'boton-agua'},
@@ -314,13 +328,10 @@ function pintarCanvas () {
         mapa.width,
         mapa.height
     )
-    lienzo.drawImage(
-        mascotaJugadorObjeto.mapaFoto,
-        mascotaJugadorObjeto.x,
-        mascotaJugadorObjeto.y,
-        mascotaJugadorObjeto.ancho,
-        mascotaJugadorObjeto.alto
-    )
+    mascotaJugadorObjeto.pintarMokepon()
+    capipepoEnemigo.pintarMokepon()
+    hipodogeEnemigo.pintarMokepon()
+    ratigueyaEnemigo.pintarMokepon()
 }
 
 function moverArriba() {
