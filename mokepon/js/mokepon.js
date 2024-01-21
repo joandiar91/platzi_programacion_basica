c080a7e2a4a1c29d18c010d185c3f33bfed682c3
Jorge Díaz <joandiar91@gmail.com>
2024-01-20 20:41:46 -0500

    Clase 80: Optimizando el mapa del juego

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -22,6 +22,7 @@ const mapa = document.getElementById("mapa")
 
 let jugadorId = null
 let mokepones = []
+let mokeponesEnemigos = []
 let inputHipodoge
 let inputCapipepo
 let inputRatigueya
@@ -374,9 +375,10 @@ function pintarCanvas () {
 
     enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y)
 
-    capipepoEnemigo.pintarMokepon()
-    hipodogeEnemigo.pintarMokepon()
-    ratigueyaEnemigo.pintarMokepon()
+    mokeponesEnemigos.forEach(function(mokepon) {
+        mokepon.pintarMokepon()
+    })
+    
     if (mascotaJugadorObjeto.velocidadX !== 0 || 
         mascotaJugadorObjeto.velocidadY !==0) {
         revisarColision(hipodogeEnemigo)
@@ -400,7 +402,7 @@ function enviarPosicion(x,y) {
                 res.json()
                     .then(function ({ enemigos }) {
                         console.log(enemigos)
-                        enemigos.forEach (function (enemigo) {
+                        mokeponesEnemigos = enemigos.map (function (enemigo) {
                             let mokeponEnemigo = null
                             const mokeponNombre = enemigo.mokepon.nombre || ""
                             if (mokeponNombre === "Hipodoge") {
@@ -414,7 +416,7 @@ function enviarPosicion(x,y) {
                             mokeponEnemigo.x = enemigo.x
                             mokeponEnemigo.y = enemigo.y
 
-                            mokeponEnemigo.pintarMokepon()
+                            return mokeponEnemigo
                         })                        
                     })
             }
